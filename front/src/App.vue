d5d7937b78823a558b5ee295e45d2064f2ab0a44
Ethan Elliott <ethan.elliott@hotmail.ca>
2019-01-26 18:14:00 -0500

    some changes

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" disable-resize-watcher floating absolute dark right app :width="drawerWidth">
-      <v-list>
-
-      </v-list>
-    </v-navigation-drawer>
     <v-toolbar app dark color="primary">
       <v-icon>filter_vintage</v-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>Scarecrow</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn>Home</v-btn>
+      <v-btn>Dials</v-btn>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
@@ -23,21 +19,7 @@
 
 export default {
   name: 'Main',
-  data: () => ({
-    drawer: null,
-    drawerWidth: window.innerWidth * 0.75
-  }),
-  mounted() {
-    this.$nextTick(() => {
-      window.addEventListener('resize', () => {
-        this.drawerWidth = window.innerWidth * 0.75
-      });
-    })
-  },
-  methods: {
-    setup(sketch) {
-      sketch.background('green')
-    }
-  }
+  data: () => ({ }),
+  mounted() { },
 }
 </script>
