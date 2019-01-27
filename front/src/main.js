d081521bfd17fb9815d30966897e861af742483e
Ethan Elliott <ethan.elliott@hotmail.ca>
2019-01-27 01:18:19 -0500

    adding tabs for dials

--- front/src/main.js
+++ front/src/main.js
@@ -4,8 +4,11 @@ import App from './App.vue'
 import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { LinearGaugePlugin } from '@syncfusion/ej2-vue-lineargauge'
 
 Vue.config.productionTip = false
+
+Vue.use(LinearGaugePlugin)
 
 new Vue({
   router,
