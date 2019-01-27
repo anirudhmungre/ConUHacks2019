23b1d35df1551f47abab962868834dd2d535285b
Ethan Elliott <ethan.elliott@hotmail.ca>
2019-01-27 10:35:37 -0500

    random values for viz of data

--- server/app.js
+++ server/app.js
@@ -8,20 +8,23 @@ let dialData = {
   humidity: Array(dataPoints).fill(0).map(e => Math.random() * 50),
   pressure: Array(dataPoints).fill(0).map(e => Math.random() * 50)
 }
-setInterval(() => {
-  dialData.temperature.shift()
-  dialData.temperature.push(Math.random() * 50)
-  dialData.humidity.shift()
-  dialData.humidity.push(Math.random() * 50)
-  dialData.pressure.shift()
-  dialData.pressure.push(Math.random() * 50)
-}, 6000)
-
 let visData = {
   tp: 0.2,
   ph: 0.2,
   sm: 0.6
 }
+setInterval(() => {
+  visData.tp = Math.random()
+  visData.ph = Math.random()
+  visData.sm = Math.random()
+  dialData.temperature.shift()
+  dialData.temperature.push(visData.tp * 50)
+  dialData.humidity.shift()
+  dialData.humidity.push(visData.ph * 50)
+  dialData.pressure.shift()
+  dialData.pressure.push(visData.ph * 50)
+
+}, 6000)
 
 socketio.on('connection', function(socket){
   console.log('a user connected')
